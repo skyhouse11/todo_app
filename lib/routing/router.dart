1cbc56c2a696b8e52b784b150b3009bb5b3ed3fa
Oleksii Kunakov <oleksii.kunakov@hotcode.lv>
2025-07-16 18:14:31 +0300

    fix: cleaned the repo for testing

--- lib/routing/router.dart
+++
@@ -1,9 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:todo_app/routing/routes.dart';
-
-part 'router.g.dart';
-
-@riverpod
-GoRouter router(Ref ref) =>
-    GoRouter(routes: $appRoutes, initialLocation: '/init');
